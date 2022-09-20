d8975050d92dfd12d15e9befee1a28f61d81f5c3
johan <johan.estrada-p@mail.escueliang.edu.co>
2022-09-19 22:51:11 -0500

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 def exp_canicas(mat,vec):
+    print('Ingrese x si quiere realizar un "click" o "N" si quiere finalizar ')
+    ins=input()
+    while ins=="x":
+        vec=acc_mat_vec(mat,vec)
+        for i in vec:
+            print(i)
+        ins = input()
+def acc_mat_vec(mat, vec):
+    """
+    matriz,vector->matriz/vector
+    retorna el resultado de el producto de un vector y una matriz
+    """
+    fil = len(mat)
+    new_vec = [0 for i in range(fil)]
+    for i in range(len(mat)):
+        valor = 0
+        for j in range(len(mat[i])):
+            valor +=mat[i][j]*vec[j]
+        new_vec[i] = valor
+    return new_vec
+
